d0e9ee45a7d6bda0689da3fb5c40797b7d72191d
SamGraber <authorsamgraber@gmail.com>
2017-02-13 09:49:41 -0500

    Setup aurelia tutorial

---
+++ app.js
@@ -0,0 +1,15 @@
+export class App {
+	
+	constructor() {
+		this.message = '';
+	}
+	
+	activate() {
+		this.message = 'Hello from Aurelia!';
+	}
+	
+	changeMessage() {
+		this.message = 'Goodbye';
+	}
+	
+}
